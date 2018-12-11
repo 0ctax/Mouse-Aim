3843e1b4578fd1dbb7968c6b12fe4937eb0ae3e2
0ctax <GoldenWolfGW@virginmedia.com>
2018-12-11 19:40:19 +0000

    Completed
    Completed the movement and made where you aim the "Player" looks.
    
    Added a toggle camera follow button.

---
+++ Follow Mouse Test/Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D rb;
+    private Vector2 MV;
+    private Vector2 Mp;
+    public GameObject cam;
+    public GameObject cube;
+    public bool FixedCam;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        Mp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 MI = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        MV = MI.normalized * speed;
+	}
+
+    void FixedUpdate()
+    {
+        Vector2 rd = Mp - rb.position;
+        float angle = Mathf.Atan2(rd.y, rd.x) * Mathf.Rad2Deg - 90f;
+        rb.MovePosition(rb.position + MV * Time.fixedDeltaTime);
+        rb.rotation = angle;
+        if (FixedCam == true) {
+            cam.transform.position = new Vector3(cube.transform.position.x, cube.transform.position.y, cube.transform.position.z - 10);
+                              }
+    }
+}
